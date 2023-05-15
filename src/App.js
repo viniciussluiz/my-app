6e880901399060c2479064c701b63acfb7781104
viniciussluiz <103579574+viniciussluiz@users.noreply.github.com>
2023-05-15 15:44:59 -0300

    adicionadas as informações do user

--- src/App.js
+++ src/App.js
@@ -1,38 +1,97 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+
 
 
 
 function App() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   const getDataApi = async () => {
-    const url = 'https://random-data-api.com/api/v2/users'
+    const url = "https://random-data-api.com/api/v2/users?size=15&is_xml=true";
     await axios.get(url)
       .then(function (response) {
-        setUsers(response.data)
+        console.log("deu certo");
+        setUsers(response.data);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
+  };
 
   useEffect(() => {
-    getDataApi()
+    getDataApi();
   }, []);
 
-  useEffect(() =>{
-  console.log(users)
+  useEffect(() => {
+    console.log(users);
   }, [users]);
 
   return (
     <>
-      <h1>teste</h1>
-      <div>
-      <p>{users.first_name}</p>
+      <h1>Users Table</h1>
+      <div className="table-container">
+      <Table className="table-bordered">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Phone Number</th>
+              <th>Date of Birth</th>
+              <th>Title</th>
+              <th>Key Skill</th>
+              <th>City</th>
+              <th>Street Name</th>
+              <th>Street Address</th>
+              <th>Zip Code</th>
+              <th>State</th>
+              <th>Country</th>
+              <th>Lat</th>
+              <th>Lng</th>
+              <th>Credit Card Number</th>
+              <th>Subscription Plan</th>
+              <th>Subscription Status</th>
+              <th>Payment Method</th>
+              <th>Subscription Term</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>{user.phone_number}</td>
+                <td>{user.date_of_birth}</td>
+                <td>{user.employment.title}</td>
+                <td>{user.employment.key_skill}</td>
+                <td>{user.address.city}</td>
+                <td>{user.address.street_name}</td>
+                <td>{user.address.street_address}</td>
+                <td>{user.address.zip_code}</td>
+                <td>{user.address.state}</td>
+                <td>{user.address.country}</td>
+                <td>{user.address.coordinates.lat}</td>
+                <td>{user.address.coordinates.lng}</td>
+                <td>{user.credit_card.cc_number}</td>
+                <td>{user.subscription.plan}</td>
+                <td>{user.subscription.status}</td>
+                <td>{user.subscription.payment_method}</td>
+                <td>{user.subscription.term}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </>
-  )
+  );
 }
+
 export default App;
